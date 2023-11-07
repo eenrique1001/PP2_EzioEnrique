7a01ba2153f3ad335d3c1edf2cf4c41af4177fd9
eenrique1001 <ezioenrique94@gmail.com>
2023-11-06 23:21:08 -0300

    commit 6

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var botao = document.querySelector("#verifica")
+    var botao = document.querySelector("#verifica");
     botao.onclick = verifica;
+    var botaolimpa = document.getElementById('limpa');
+    botaolimpa.onclick = limpa;
+    var botaolimpa2 = document.getElementById('limpa2');
+    botaolimpa2.onclick = limpa;
+
+
 });
+
+function limpa() {
+    const spans = document.querySelectorAll('span');
+        for(let span of spans){
+            span.textContent = "";
+        }
+        const node = document.getElementById('escondida');
+        node.style.display = 'none';
+}
 
 function verifica(){
 
@@ -10,7 +25,8 @@ function verifica(){
     const formulario = document.querySelector('form');
     const nome = document.getElementById('nome');
     const email = document.getElementById('email');
-    const checks = document.querySelectorAll("input[name='checks']")
+    const checks = document.querySelectorAll("input[name='checks']");
+    const radio = document.querySelector("input[name='opcoes']:checked");
     const texto = document.getElementById('areaTexti');
 
     const spans = document.querySelectorAll('span');
@@ -20,29 +36,21 @@ function verifica(){
     const spanCheck = spans[2];
     const spanText = spans[3];
 
-    spanNome.textContent = "✔";
-    spanEmail.textContent = "✔";
-    spanCheck.textContent = "✔";
-    spanText.textContent = "✔";
-
     for(let span of spans){
+        span.textContent = "✔";
         span.style.color = 'red';
     }
 
     nome.value = escapeInput(nome.value)
     email.value = escapeInput(email.value)
     texto.value = escapeInput(texto.value)
-
-    nome.value.toLowerCase();
-    email.value.toLowerCase();
-    texto.value.toLowerCase();
        
     if(nome.value.length > 100 ) {
         spanNome.textContent = 'O nome não pode conter mais de 100 caracteres';
         flag = false;
     }
 
-    if(nome.value.length == 0) {
+    else if(nome.value.length == 0) {
         spanNome.textContent = 'Por favor insira seu nome';
         flag = false;
     }
@@ -50,33 +58,33 @@ function verifica(){
 
     var emaillength = email.value.length;
 
+
     if(emaillength > 100 ) {
         spanEmail.textContent = 'O email não pode conter mais de 100 caracteres';
         flag = false;
     }
 
-    if(emaillength == 0) {
+    else if(emaillength == 0) {
         spanEmail.textContent = 'Por favor insira seu email';
         flag = false;
     }
-    else { 
-        if (!email.value.includes('@', 1) || !email.value.includes('.', email.value.length-1)) {
-            spanEmail.textContent = 'Por favor insira um email válido';
-            flag = false;
-        }
-        else if(email.value.indexOf('@') > email.value.indexOf('.')){
-            spanEmail.textContent = 'Por favor insira um email válido';
-            flag = false;
-        }
+    else if (!email.value.includes('@') || !email.value.includes('.')) {
+        spanEmail.textContent = 'Por favor insira um email válido';
+        flag = false;
     }
+    else if(email.value.indexOf('@') > email.value.indexOf('.') || email.value.indexOf('@') == 0 || email.value.indexOf('.') == emaillength-1) {
+        spanEmail.textContent = 'Por favor insira um email válido';
+        flag = false;
+    }
+    
 
     const opcoesSelecionadas = [];
     let flag1 = false;
     for(let check of checks){
         if(check.checked){
             flag1 = true;
-            ocument.querySelector(`label[for="${check.id}"]`)
-            opcoesSelecionadas.push(label);
+            label = document.querySelector('label[for="'+check.id+'"]');
+            opcoesSelecionadas.push(label.textContent);
         }
     }
     if(!flag1){
@@ -94,9 +102,37 @@ function verifica(){
         flag = false;
     }
 
+    const opcao = radio.nextElementSibling.textContent;
+    
+    if(flag){
+        apresentar(nome.value, email.value, opcoesSelecionadas, opcao, texto.value);
+        flag = false;
+    }
     
 
-    
+    return flag;
+}
+
+function apresentar(nome, email, assunto, opcoes, texto){
+    const flag = false;
+
+    const node = document.getElementById('escondida');
+    node.style.display = 'block';
+    const span = document.querySelectorAll('span[name="spans"]');
+
+    for(let spans of span){
+        spans.style.color = 'rgb(0, 112, 97)';
+    }
+
+    span[0].textContent = nome;
+    span[1].textContent = email;
+    var assuntospan = "";
+    for(let assuntos of assunto){
+        assuntospan += "<br>"+assuntos;
+    }
+    span[2].innerHTML = assuntospan;
+    span[3].textContent = opcoes;
+    span[4].textContent = texto;
 
     return flag;
 }
